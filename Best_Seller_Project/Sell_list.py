e97bef5450abcd788241e4c5f1fa3a1dffa45e28
Ricardo Silva <46853876+caldeira11@users.noreply.github.com>
2023-05-24 12:23:24 +0100

    Add files via upload

---
+++ Best_Seller_Project/Sell_list.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
+for mes in lista_meses:
+    tabela_vendas = pd.read_excel(f'{mes}.xlsx')
+    if (tabela_vendas['Vendas'] > 55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
+        print(f'No mês de {mes} encontrou {vendedor} com {vendas}€ em vendas.')
